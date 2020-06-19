b448ccf68e3c0e84210f48d2053f2b17dcd47f0e
divyanshrana <divyansh.rana12@gmail.com>
2020-06-19 17:05:16 +0530

    Updates

---
+++ precache-manifest.dd69b876835c5a9dc34e56fbbd8cf420.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "785afaa895e43c277d06f62ceafd4169",
+    "url": "/Calculator/index.html"
+  },
+  {
+    "revision": "6deccb127313d1d3f21e",
+    "url": "/Calculator/static/css/main.a4191227.chunk.css"
+  },
+  {
+    "revision": "8773711676a4718f38f2",
+    "url": "/Calculator/static/js/2.8e5e1336.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Calculator/static/js/2.8e5e1336.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6deccb127313d1d3f21e",
+    "url": "/Calculator/static/js/main.84a8ced7.chunk.js"
+  },
+  {
+    "revision": "2b5cbcab57f22a4d9621",
+    "url": "/Calculator/static/js/runtime-main.6cfc3ab0.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Calculator/static/media/logo.5d5d9eef.svg"
+  }
+]);
